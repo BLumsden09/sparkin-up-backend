53a3690c92be480bff9b848fb565ba4adea00f58
Brandon <bpl5@students.uwf.edu>
2018-04-30 08:04:00 -0400

    User registration page now saves User data into the database using the GraphQL register mutation. Next in line: Email account verification, Visual remake of the Register page, Form validation, and Login user functionality with JWT.

--- resolvers.js
+++ resolvers.js
@@ -1,13 +1,49 @@
 const mongoose = require('mongoose')
 const userModel = require('./models/user')
+const ObjectId = require('mongodb').ObjectId;
+const bcrypt = require('bcrypt')
+const config = require('./config/database');
+const jwt = require('jsonwebtoken')
 
 const resolvers = {
     Query: {
         allUsers: (root, {searchTerm}) => {
-            if (searchTerm !== '')
-                return userModel.find({$text: {$search: searchTerm}}).sort({lastName: 'asc'});
-            else
-                return userModel.find().sort({lastName: 'asc'});
+          if (searchTerm !== '')
+              return userModel.find({$text: {$search: searchTerm}}).sort({lastName: 'asc'});
+          else
+              return userModel.find().sort({lastName: 'asc'});
+        },
+        getUser: (root, {_id}) =>{
+          return userModel.findOne(ObjectId(_id));
+        }
+    },
+    Mutation: {
+        register: async (root, args) => {
+          const user = args;
+          user.password.plaintext = await bcrypt.hash(user.password, 12);
+          debugger;
+          return userModel.create(user);
+        },
+        deleteUser: (root, {_id}) => {
+          return userModel.deleteOne({_id: ObjectId(_id)});
+        },
+        updateUser: async (root, {_id, changedUser}) => {
+          changedUser.password = await bcrypt.hash(changedUser.password, 12);
+          const userValues = {$set: {email: changedUser.email, password: changedUser.password,
+                              firstName: changedUser.firstName, lastName: changedUser.lastName}};
+          return userModel.findOneAndUpdate({_id: ObjectId(_id)}, userValues);
+        },
+        login: async (root, {email, password}) => {
+          const user = await userModel.findOne({ email: email });
+          if (!user){
+            throw new Error('No user found with that email');
+          }
+          const valid = await bcrypt.compare(password, user.password);
+          if (!valid){
+            throw new Error('Incorrect password ' + password + user.password);
+          }
+          const token = jwt.sign({ userId: user._id }, config.secret, { expiresIn: "1 day" });
+          return token;
         }
     }
 }
--- schema.js
+++ schema.js
@@ -1,16 +1,39 @@
+const mongoose = require('mongoose')
 const ExecutableSchema = require('graphql-tools')
 const resolvers = require('./resolvers')
 
 const typeDefs = [`
     type User {
-        email: String
-        password: String
-        firstName: String
-        lastName: String
+        _id: String!
+        email: String!
+        password: String!
+        firstName: String!
+        lastName: String!
         skills: [String]
     }
+
+    type LoginObject {
+        secret: String!
+        token: String
+    }
+
+    input userChanges {
+        email: String!
+        password: String!
+        firstName: String!
+        lastName: String!
+        skills: [String]
+    }
+
     type Query {
-        allUsers(searchTerm: String): [User]
+        allUsers(searchTerm: String): [User!]!
+        getUser(_id: String!): User
+    }
+    type Mutation {
+        deleteUser(_id: String!): User
+        updateUser(_id: String!, changedUser: userChanges): User!
+        login(email: String!, password: String!): String!
+        register(email: String!, password: String!, firstName: String!, lastName: String!): User!
     }
 `];
 
